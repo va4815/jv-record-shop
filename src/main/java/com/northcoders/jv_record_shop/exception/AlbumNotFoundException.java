4119afcb6e899b92cf65246d32991ce6ee4f651f
Vincent <va543567@gmail.com>
2024-12-05 11:12:42 +0000

    feat: added album not found exception

---
+++ src/main/java/com/northcoders/jv_record_shop/exception/AlbumNotFoundException.java
@@ -0,0 +1,8 @@
+package com.northcoders.jv_record_shop.exception;
+
+public class AlbumNotFoundException extends RuntimeException {
+
+    public AlbumNotFoundException(String message) {
+        super(message);
+    }
+}
