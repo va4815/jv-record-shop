ee5e02499d050b9a05a93d471a3ef8fb56571bd0
Vincent <va543567@gmail.com>
2024-12-05 09:33:02 +0000

    feat: record shop project initialisation

---
+++ src/main/java/com/northcoders/jv_record_shop/JvRecordShopApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.jv_record_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvRecordShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvRecordShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/jv_record_shop/JvRecordShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.jv_record_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvRecordShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
