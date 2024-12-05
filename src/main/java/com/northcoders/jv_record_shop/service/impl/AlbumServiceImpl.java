576311749f3e3a74a27223be363c5423d6ad1f07
Vincent <va543567@gmail.com>
2024-12-05 11:47:00 +0000

    feat: implement get all albums service

--- src/main/java/com/northcoders/jv_record_shop/service/impl/AlbumServiceImpl.java
+++ src/main/java/com/northcoders/jv_record_shop/service/impl/AlbumServiceImpl.java
@@ -1,17 +1,24 @@
 package com.northcoders.jv_record_shop.service.impl;
 
 import com.northcoders.jv_record_shop.model.Album;
+import com.northcoders.jv_record_shop.repository.AlbumRepository;
 import com.northcoders.jv_record_shop.service.AlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class AlbumServiceImpl implements AlbumService {
 
+    @Autowired
+    private AlbumRepository albumRepository;
+
     @Override
     public List<Album> getAllAlbums() {
-        // TODO: implement get all albums
-        return List.of();
+        List<Album> albums = new ArrayList<>();
+        albumRepository.findAll().forEach(albums::add);
+        return albums;
     }
 }
