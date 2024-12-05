ccece5af0389e603410299232dbd438225626d4c
Vincent <va543567@gmail.com>
2024-12-05 16:47:19 +0000

    feat: implement get all songs test case in repository layer

--- src/test/java/com/northcoders/jv_record_shop/repository/SongRepositoryTest.java
+++ src/test/java/com/northcoders/jv_record_shop/repository/SongRepositoryTest.java
@@ -1,12 +1,39 @@
 package com.northcoders.jv_record_shop.repository;
 
+import com.northcoders.jv_record_shop.model.Song;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 @DataJpaTest
 public class SongRepositoryTest {
 
     @Autowired
     private SongRepository songRepository;
+
+    @Test
+    public void testFindAllSongs() {
+        List<Song> songs = Arrays.asList(
+                Song.builder()
+                        .title("Fearless")
+                        .writer("Taylor Swift")
+                        .songLength(Duration.ofSeconds(241))
+                        .build()
+        );
+
+        songRepository.saveAll(songs);
+
+        Iterable<Song> results = songRepository.findAll();
+
+        Assertions.assertThat(results)
+                .isNotNull()
+                .hasSameSizeAs(songs);
+
+    }
 
 }
