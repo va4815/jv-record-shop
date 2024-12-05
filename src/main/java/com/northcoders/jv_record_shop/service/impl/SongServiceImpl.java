05b75339b406698ff1a63f4639b4fcb43fd3e98c
Vincent <va543567@gmail.com>
2024-12-05 16:35:44 +0000

    feat: added song service file

---
+++ src/main/java/com/northcoders/jv_record_shop/service/impl/SongServiceImpl.java
@@ -0,0 +1,8 @@
+package com.northcoders.jv_record_shop.service.impl;
+
+import com.northcoders.jv_record_shop.service.SongService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SongServiceImpl implements SongService {
+}
