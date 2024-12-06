43d6e360729e6c94bc5f75aa74348fb6656a5cff
Vincent <va543567@gmail.com>
2024-12-06 11:04:14 +0000

    feat: implemented create artist test case in service layer

--- src/test/java/com/northcoders/jv_record_shop/service/ArtistsServiceTest.java
+++ src/test/java/com/northcoders/jv_record_shop/service/ArtistsServiceTest.java
@@ -1,13 +1,16 @@
 package com.northcoders.jv_record_shop.service;
 
+import com.northcoders.jv_record_shop.dto.request.CreateArtistsRequestDTO;
 import com.northcoders.jv_record_shop.model.Artists;
 import com.northcoders.jv_record_shop.model.Gender;
+import com.northcoders.jv_record_shop.model.Song;
 import com.northcoders.jv_record_shop.repository.ArtistsRepository;
 import com.northcoders.jv_record_shop.service.impl.ArtistsServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Arrays;
@@ -56,6 +59,27 @@ public class ArtistsServiceTest {
         when(mockArtistsRepository.findById(1L)).thenReturn(Optional.ofNullable(artist));
 
         Artists result = artistsServiceImpl.getArtistById(1L);
+
+        assertThat(result).isEqualTo(artist);
+
+    }
+
+    @Test
+    @DisplayName("add artist with valid data")
+    void testAddArtistWithValidData() {
+        Artists artist = Artists.builder()
+                .name("Taylor Swift")
+                .gender(Gender.F)
+                .build();
+
+        CreateArtistsRequestDTO requestDTO = CreateArtistsRequestDTO.builder()
+                .name("Taylor Swift")
+                .gender(Gender.F)
+                .build();
+
+        when(mockArtistsRepository.save(Mockito.any(Artists.class))).thenReturn(artist);
+
+        Artists result = artistsServiceImpl.createArtist(requestDTO);
 
         assertThat(result).isEqualTo(artist);
 
