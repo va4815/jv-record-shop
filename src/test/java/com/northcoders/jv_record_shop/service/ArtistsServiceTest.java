50e33a0a020ce2946ee0a73903cf020bff77e12d
Vincent <va543567@gmail.com>
2024-12-06 11:01:27 +0000

    feat: implemented get all artists test case in service layer

--- src/test/java/com/northcoders/jv_record_shop/service/ArtistsServiceTest.java
+++ src/test/java/com/northcoders/jv_record_shop/service/ArtistsServiceTest.java
@@ -1,18 +1,48 @@
 package com.northcoders.jv_record_shop.service;
 
-import com.northcoders.jv_record_shop.repository.ArtistsRepositoryTest;
+import com.northcoders.jv_record_shop.model.Artists;
+import com.northcoders.jv_record_shop.model.Gender;
+import com.northcoders.jv_record_shop.repository.ArtistsRepository;
 import com.northcoders.jv_record_shop.service.impl.ArtistsServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @DataJpaTest
 public class ArtistsServiceTest {
 
     @Mock
-    private ArtistsRepositoryTest mockArtistsRepository;
+    private ArtistsRepository mockArtistsRepository;
 
     @InjectMocks
     private ArtistsServiceImpl artistsServiceImpl;
+
+    @Test
+    @DisplayName("get all artists")
+    void testGetAllArtists() {
+        List<Artists> artists = Arrays.asList(
+                Artists.builder()
+                        .name("Taylor Swift")
+                        .gender(Gender.F)
+                        .build()
+        );
+
+        when(mockArtistsRepository.findAll()).thenReturn(artists);
+
+        List<Artists> actualResult = artistsServiceImpl.getAllArtists();
+
+        assertThat(actualResult).hasSize(1);
+        assertThat(actualResult).isEqualTo(artists);
+
+    }
 
 }
