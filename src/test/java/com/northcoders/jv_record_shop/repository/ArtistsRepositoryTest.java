3b2107962c77e3f14b7f9659a0c421f412582048
Vincent <va543567@gmail.com>
2024-12-06 10:38:22 +0000

    feat: implemented find all artists in repository layer

--- src/test/java/com/northcoders/jv_record_shop/repository/ArtistsRepositoryTest.java
+++ src/test/java/com/northcoders/jv_record_shop/repository/ArtistsRepositoryTest.java
@@ -1,12 +1,37 @@
 package com.northcoders.jv_record_shop.repository;
 
+import com.northcoders.jv_record_shop.model.Artists;
+import com.northcoders.jv_record_shop.model.Gender;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Arrays;
+import java.util.List;
 
 @DataJpaTest
 public class ArtistsRepositoryTest {
 
     @Autowired
     private ArtistsRepository artistsRepository;
+
+    @Test
+    public void testFindAllArtists() {
+        List<Artists> artists = Arrays.asList(
+                Artists.builder()
+                        .name("Taylor Swift")
+                        .gender(Gender.F)
+                        .build()
+        );
+
+        artistsRepository.saveAll(artists);
+
+        Iterable<Artists> results = artistsRepository.findAll();
+
+        Assertions.assertThat(results)
+                .isNotNull()
+                .hasSameSizeAs(artists);
+    }
 
 }
