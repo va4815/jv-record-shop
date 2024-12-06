a37bb0eba6243bda3316baaf55c15c87ca51c758
Vincent <va543567@gmail.com>
2024-12-06 13:47:05 +0000

    feat: implemented get all artists API and test case

--- src/main/java/com/northcoders/jv_record_shop/controller/ArtistsController.java
+++ src/main/java/com/northcoders/jv_record_shop/controller/ArtistsController.java
@@ -1,9 +1,29 @@
 package com.northcoders.jv_record_shop.controller;
 
+import com.northcoders.jv_record_shop.dto.response.ArtistsResponseDTO;
+import com.northcoders.jv_record_shop.model.Artists;
+import com.northcoders.jv_record_shop.service.ArtistsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("artists")
+@RequestMapping("/api/v1/artists")
 public class ArtistsController {
+
+    @Autowired
+    private ArtistsService artistsService;
+
+    @GetMapping
+    public ResponseEntity<List<ArtistsResponseDTO>> getAllArtists() {
+        List<Artists> artists = artistsService.getAllArtists();
+        List<ArtistsResponseDTO> artistsDTOList = artists.stream().map(ArtistsResponseDTO::new).toList();
+        return new ResponseEntity<>(artistsDTOList, HttpStatus.OK);
+    }
+
 }
--- src/test/java/com/northcoders/jv_record_shop/controller/ArtistsControllerTest.java
+++ src/test/java/com/northcoders/jv_record_shop/controller/ArtistsControllerTest.java
@@ -1,15 +1,26 @@
 package com.northcoders.jv_record_shop.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.northcoders.jv_record_shop.model.Artists;
+import com.northcoders.jv_record_shop.model.Gender;
 import com.northcoders.jv_record_shop.service.impl.ArtistsServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -30,6 +41,31 @@ public class ArtistsControllerTest {
     public void setup() {
         mockMvcController = MockMvcBuilders.standaloneSetup(artistsController).build();
         mapper = new ObjectMapper();
+    }
+
+    @Test
+    @DisplayName("GET /artists")
+    void getAllArtists() throws Exception {
+        List<Artists> artists = Arrays.asList(
+                Artists.builder()
+                        .name("Taylor Swift")
+                        .gender(Gender.F)
+                        .build(),
+                Artists.builder()
+                        .name("Yoshiki")
+                        .gender(Gender.M)
+                        .build()
+        );
+
+        when(this.mockArtistsServiceImpl.getAllArtists()).thenReturn(artists);
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/api/v1/artists"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Taylor Swift"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("Yoshiki"))
+        ;
+
     }
 
 }
