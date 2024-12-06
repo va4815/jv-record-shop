8bc4e6194009e45f3adf68e519a18148079c3c88
Vincent <va543567@gmail.com>
2024-12-06 10:50:23 +0000

    feat: added get all artists method in service

--- src/main/java/com/northcoders/jv_record_shop/service/ArtistsService.java
+++ src/main/java/com/northcoders/jv_record_shop/service/ArtistsService.java
@@ -1,4 +1,9 @@
 package com.northcoders.jv_record_shop.service;
 
+import com.northcoders.jv_record_shop.model.Artists;
+
+import java.util.List;
+
 public interface ArtistsService {
+    List<Artists> getAllArtists();
 }
--- src/main/java/com/northcoders/jv_record_shop/service/impl/ArtistsServiceImpl.java
+++ src/main/java/com/northcoders/jv_record_shop/service/impl/ArtistsServiceImpl.java
@@ -1,8 +1,22 @@
 package com.northcoders.jv_record_shop.service.impl;
 
+import com.northcoders.jv_record_shop.model.Artists;
+import com.northcoders.jv_record_shop.repository.ArtistsRepository;
 import com.northcoders.jv_record_shop.service.ArtistsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ArtistsServiceImpl implements ArtistsService {
+
+    @Autowired
+    private ArtistsRepository artistsRepository;
+
+    @Override
+    public List<Artists> getAllArtists() {
+        // TODO: get all artists
+        return List.of();
+    }
 }
