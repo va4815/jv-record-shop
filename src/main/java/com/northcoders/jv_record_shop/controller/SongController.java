fe1cd2d44dbcacf69caf13b44598431aa454ba13
Vincent <va543567@gmail.com>
2024-12-13 20:05:51 +0000

    feat: implemented get song by id API

--- src/main/java/com/northcoders/jv_record_shop/controller/SongController.java
+++ src/main/java/com/northcoders/jv_record_shop/controller/SongController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,15 @@ public class SongController {
         List<Song> songs = songService.getAllSongs();
         List<SongResponseDTO> songDTOList = songs.stream().map(SongResponseDTO::new).toList();
         return new ResponseEntity<>(songDTOList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SongResponseDTO> getSongById(@PathVariable(name = "id") Long id) {
+        Song song = songService.getSongById(id);
+        if (song == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(new SongResponseDTO(song), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/northcoders/jv_record_shop/dto/response/SongResponseDTO.java
+++ src/main/java/com/northcoders/jv_record_shop/dto/response/SongResponseDTO.java
@@ -7,12 +7,14 @@ import java.time.Duration;
 
 @Data
 public class SongResponseDTO {
+    private Long id;
     private String title;
     private String writer;
     private long songLength;
 
     public SongResponseDTO(Song song) {
         if (song != null) {
+            this.id = song.getId();
             this.title = song.getTitle();
             this.writer = song.getWriter();
             this.songLength = song.getSongLength().toSeconds();
